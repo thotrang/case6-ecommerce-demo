615df9db622fad16f5392ab29b7ee924e1666f22
nguyen-van-thu <102147814+nguyen-van-thu@users.noreply.github.com>
2022-09-26 11:52:44 +0700

    model-store

--- src/model/store.js
+++ src/model/store.js
@@ -1,2 +1,50 @@
+const mongoose = require('mongoose')
+const {model} = require("mongoose");
+const Schema = mongoose.Schema
+
+const StoreSchema = new Schema({
+    UserId : {
+        type : Schema.Types.ObjectId,
+        ref : ' user',
+        require : true
+    },
+    Name : {
+        type : String,
+        require: true
+    },
+    Image : [
+        {
+            public_id : {
+                type : String,
+                require : true
+            },
+            url : {
+                type : String,
+                require : true
+            }
+        }
+    ],
+
+    ProductsId : {
+        type: Schema.Types.ObjectId,
+        ref :'product'
+    },
+    CategoryId : {
+        type: Schema.Types.ObjectId,
+        ref : 'category'
+    },
+    Date : {
+        type : Date,
+        default: Date.now
+    },
+    Rating : {
+        type : Number,
+        default : 0
+    }
+
+})
+module.exports = mongoose.model('store',StoreSchema)
+
+
 
 
